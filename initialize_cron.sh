791d98e5b291f55a55926954412ac0eb9a87fd79
hooray4me <robhedrick@outlook.com>
2023-05-14 13:33:08 -0500

    update initialize_cron.sh with conditional logic

--- initialize_cron.sh
+++ initialize_cron.sh
@@ -2,8 +2,19 @@
 printenv | sed 's/^\(.*\)$/export \1/g' > /root/project_env.sh
 chmod +x /root/project_env.sh
 touch /logs/cron.log
-cd /m3u2strm && python3 main.py $TVSHOWURL tv /tv/ $APOLLO
-cd /m3u2strm && python3 main.py $MOVIEURL movies /movies/ $APOLLO
-(crontab -l ; echo "$TVCRONMINUTE $TVCRONHOUR * * * . /root/project_env.sh; cd /m3u2strm && python3 main.py $TVSHOWURL tv /tv/ $APOLLO >> /logs/cron.log") | crontab
-(crontab -l ; echo "$MOVIECRONMINUTE $MOVIECRONHOUR * * * . /root/project_env.sh; cd /m3u2strm && python3 main.py $MOVIEURL movies /movies/ $APOLLO >> /logs/cron.log") | crontab
+if [ "$TVSHOWS" == "true" ]
+then
+    cd /m3u2strm && python3 main.py $TVSHOWURL tv /tv/ $APOLLO
+    (crontab -l ; echo "$TVCRONMINUTE $TVCRONHOUR * * * . /root/project_env.sh; cd /m3u2strm && python3 main.py $TVSHOWURL tv /tv/ $APOLLO >> /logs/cron.log") | crontab
+fi
+if [ "$MOVIES" == "true" ]
+then
+    cd /m3u2strm && python3 main.py $MOVIEURL movies /movies/ $APOLLO
+    (crontab -l ; echo "$MOVIECRONMINUTE $MOVIECRONHOUR * * * . /root/project_env.sh; cd /m3u2strm && python3 main.py $MOVIEURL movies /movies/ $APOLLO >> /logs/cron.log") | crontab
+fi
+if [ "$EVENTS" == "true" ]
+then
+    cd /m3u2strm && python3 main.py $EVENTURL events /events/ $APOLLO
+    (crontab -l ; echo "$EVENTCRONMINUTE $EVENTCRONHOUR * * * . /root/project_env.sh; cd /m3u2strm && python3 main.py $EVENTURL events /events/ $APOLLO >> /logs/cron.log") | crontab
+fi
 cron && tail -f /logs/cron.log
