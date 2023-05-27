2ccf1734720697507d8a8ee703c9303b629d3a93
Jurgen Heine <jurgenheine@hotmail.com>
2023-05-27 12:06:30 +0200

    Fix CRON problems

--- initialize_cron.sh
+++ initialize_cron.sh
@@ -1,29 +1,62 @@
 #! /bin/bash
-printenv | sed 's/^\(.*\)$/export \1/g' > /root/project_env.sh
+# store env as export in script file to export, filter LS_color env, it interfere with the export command
+printenv | compgen -v -X '*LS_COLORS*' | sed 's/^\(.*\)$/export \1/g' > /root/project_env.sh
 chmod +x /root/project_env.sh
+
+#create log file
 touch /logs/cron.log
 
+# remove old jobs, needed when docker restarted, otherwise there multiple rules created
+crontab -r
+
+#create cron rule(s)
 if [ "$SINGLELIST" == "true" ]
 then
     cd /m3u2strm && python3 main.py $SINGLELISTURL all $MOVIES $TVSHOWS $EVENTS /movies/ /tv/ /events/
-    (crontab -l ; echo "$CRONMINUTE $CRONHOUR * * * . /root/project_env.sh; cd /m3u2strm && python3 main.py $SINGLELISTURL all $MOVIES $TVSHOWS $EVENTS /movies/ /tv/ /events/ >> /logs/cron.log") | crontab
-fi
-if  [ "$SINGLELIST" != "true" ]
-then
-    if [ "$TVSHOWS" == "true" ]
+    if [ -z "$CRON" ]
     then
+        # CRON variable empty, use the hour minute
+        (crontab -l ; echo "$CRONMINUTE $CRONHOUR * * * . /root/project_env.sh; cd /m3u2strm && python3 main.py $SINGLELISTURL all $MOVIES $TVSHOWS $EVENTS /movies/ /tv/ /events/ >> /logs/cron.log") | crontab
+    else
+        # Use FULL CRON, so you can shedule like you want
+        (crontab -l ; echo "$CRON . /root/project_env.sh; cd /m3u2strm && python3 main.py $SINGLELISTURL all $MOVIES $TVSHOWS $EVENTS /movies/ /tv/ /events/ >> /logs/cron.log") | crontab
+    fi
+else
+    if [ "$TVSHOWS" == "true" ]
+    then 
         cd /m3u2strm && python3 main.py $TVSHOWURL tvshows $APOLLO /tv/
-        (crontab -l ; echo "$TVCRONMINUTE $TVCRONHOUR * * * . /root/project_env.sh; cd /m3u2strm && python3 main.py $TVSHOWURL tvshows $APOLLO /tv/ >> /logs/cron.log") | crontab
+        if [ -z "$TVCRON" ]
+        then
+            # CRON variable empty, use the hour minute
+            (crontab -l ; echo "$TVCRONMINUTE $TVCRONHOUR * * * . /root/project_env.sh; cd /m3u2strm && python3 main.py $TVSHOWURL tvshows $APOLLO /tv/ >> /logs/cron.log") | crontab
+        else
+            # Use FULL CRON, so you can shedule like you want
+            (crontab -l ; echo "$TVCRON . /root/project_env.sh; cd /m3u2strm && python3 main.py $TVSHOWURL tvshows $APOLLO /tv/ >> /logs/cron.log") | crontab
+        fi
     fi
     if [ "$MOVIES" == "true" ]
     then
         cd /m3u2strm && python3 main.py $MOVIEURL movies $APOLLO /movies/
-        (crontab -l ; echo "$MOVIECRONMINUTE $MOVIECRONHOUR * * * . /root/project_env.sh; cd /m3u2strm && python3 main.py $MOVIEURL movies $APOLLO /movies/ >> /logs/cron.log") | crontab
+        if [ -z "$MOVIECRON" ]
+        then
+            # CRON variable empty, use the hour minute
+            (crontab -l ; echo "$MOVIECRONMINUTE $MOVIECRONHOUR * * * . /root/project_env.sh; cd /m3u2strm && python3 main.py $MOVIEURL movies $APOLLO /movies/ >> /logs/cron.log") | crontab
+        else
+            # Use FULL CRON, so you can shedule like you want
+            (crontab -l ; echo "$MOVIECRON . /root/project_env.sh; cd /m3u2strm && python3 main.py $MOVIEURL movies $APOLLO /movies/ >> /logs/cron.log") | crontab
+        fi
     fi
     if [ "$EVENTS" == "true" ]
     then
         cd /m3u2strm && python3 main.py $EVENTURL events $APOLLO /events/
-        (crontab -l ; echo "$EVENTCRONMINUTE $EVENTCRONHOUR * * * . /root/project_env.sh; cd /m3u2strm && python3 main.py $EVENTURL events $APOLLO /events/ >> /logs/cron.log") | crontab
+        if [ -z "$EVENTCRON" ]
+        then
+            # CRON variable empty, use the hour minute
+            (crontab -l ; echo "$EVENTCRONMINUTE $EVENTCRONHOUR * * * . /root/project_env.sh; cd /m3u2strm && python3 main.py $EVENTURL events $APOLLO /events/ >> /logs/cron.log") | crontab
+        else
+            # Use FULL CRON, so you can shedule like you want
+            (crontab -l ; echo "$EVENTCRON . /root/project_env.sh; cd /m3u2strm && python3 main.py $EVENTURL events $APOLLO /events/ >> /logs/cron.log") | crontab
+        fi
     fi
 fi
 cron && tail -f /logs/cron.log
